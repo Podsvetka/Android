6d83833b1a2226621fbe4e11fbafb9c3fdbee4ec
Svetlana <s.y.topchiy@student.khai.edu>
2025-03-13 23:23:18 +0200

    Add files via upload

---
+++ Pr3.kt
@@ -0,0 +1,27 @@
+import kotlin.random.Random
+
+fun main() {
+    val options = listOf("Камінь", "Ножиці", "Папір")
+    val computerChoice = options[Random.nextInt(options.size)]
+
+    println("Комп'ютер обрав: $computerChoice")
+
+    var userChoice: String?
+    do {
+        println("Оберіть: Камінь, Ножиці или Папір")
+        userChoice = readLine()?.capitalize()
+        if (userChoice !in options) {
+            println("Помилка! Будь ласка, виберіть одне із запропонованих слів.")
+        }
+    } while (userChoice !in options)
+
+    when {
+        userChoice == computerChoice -> println("Нічия!")
+        userChoice == "Камінь" && computerChoice == "Ножиці" ||
+                userChoice == "Ножницы" && computerChoice == "Папір" ||
+                userChoice == "Папір" && computerChoice == "Камінь" ->
+            println("Ви виграли!")
+        else -> println("Комп'ютер виграв!")
+    }
+}
+
